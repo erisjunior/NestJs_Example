f1d81e8a6ae050c15f4f0e4ea592a7d1902c1e40
Erisvan Junior <erisvan.junior.a@gmail.com>
2019-11-05 04:30:36 -0300

    feat: database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { DogsModule } from './dogs/dogs.module';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
+import { Photo } from './photos/photo.entity';
+import { PhotoModule } from './photos/photo.module';
+
 @Module({
-  imports: [CatsModule, DogsModule],
+  imports: [
+    CatsModule,
+    DogsModule,
+    PhotoModule,
+    TypeOrmModule.forRoot({entities: [Photo]})
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
